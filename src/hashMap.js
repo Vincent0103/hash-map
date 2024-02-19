dde5b747c70927c2e71eb2e0e6f943a2d1838c54
Vincent0103 <vincentkanagasundaram@gmail.com>
2024-02-19 08:22:50 +0100

    Add hashMap with hash,set sub-functions

--- src/hashMap.js
+++ src/hashMap.js
@@ -1,1 +1,23 @@
-console.log('Hello world!');
+const HashMap = () => {
+  let buckets;
+
+  const hash = (key) => {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i += 1) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+      if (hashCode > buckets.length) hashCode %= buckets.length;
+    }
+
+    return hashCode;
+  };
+
+  const set = (key, value) => {
+    const index = hashCode(key);
+
+    if (index < 0 || index >= buckets.length) {
+      throw new Error('Trying to access index out of bound');
+    }
+  };
+};
