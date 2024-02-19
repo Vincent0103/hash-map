0a6c5af218b20d94a2f8b804a7fcdb6a38c5fe09
Vincent0103 <vincentkanagasundaram@gmail.com>
2024-02-19 19:23:03 +0100

    Fix bad set function implementation

--- src/hashMap.js
+++ src/hashMap.js
@@ -1,10 +1,10 @@
 const HashMap = () => {
-  const buckets = new Array(16);
+  const buckets = new Array(16).fill(null);
   const LOAD_FACTOR = 0.75;
 
-  const node = (key, value, next = null) => ({ key, value, next });
+  const node = (key, data, next = null) => ({ key, data, next });
 
-  const hash = (key) => {
+  function hash(key) {
     let hashCode = 0;
 
     const primeNumber = 31;
@@ -14,10 +14,10 @@ const HashMap = () => {
     }
 
     return hashCode;
-  };
+  }
 
-  const calculateLoadFactor = () => {
-    const itemsAmount = buckets.filter((item) => item.key).reduce((acc, curr) => {
+  function calculateLoadFactor() {
+    const itemsAmount = buckets.filter((item) => item?.key).reduce((acc, curr) => {
       let currentNode = curr;
       let accumulator = acc;
       while (currentNode) {
@@ -27,43 +27,48 @@ const HashMap = () => {
       return accumulator;
     }, 0);
 
-    return itemsAmount / buckets.length;
-  };
+    return itemsAmount;
+  }
 
-  const getNode = (key, currentNode = buckets[hash(key)]) => {
+  function getNode(key, currentNode = buckets[hash(key)]) {
     if (!currentNode) return null;
     if (key === currentNode.key) return currentNode;
     if (currentNode.next) return getNode(key, currentNode.next);
     return null;
-  };
+  }
 
-  const get = (key) => {
+  function get(key) {
     const currentNode = getNode(key);
     if (!currentNode) return null;
     return currentNode.value;
-  };
+  }
 
-  const has = (key) => {
+  function has(key) {
     const currentNode = getNode(key);
     if (!currentNode) return false;
     return true;
-  };
+  }
 
-  const set = (key, value) => {
+  function set(key, value) {
     const index = hash(key);
-    const currentBucket = buckets[index];
-    if (currentBucket && currentBucket.key === key) {
-      currentBucket.value = value;
-    } else if (currentBucket && currentBucket.key !== key) {
-      currentBucket.next = node(key, value);
-    } else {
+    let currentNode = buckets[index];
+    if (!currentNode) {
       buckets[index] = node(key, value);
+      return;
     }
-  };
-
-  const remove = (key) => {
-
-  };
+    if (currentNode.key === key) {
+      currentNode.value = value;
+      return;
+    }
+    while (currentNode.next) {
+      currentNode = currentNode.next;
+      if (currentNode.key === key) {
+        currentNode.value = value;
+        return;
+      }
+    }
+    currentNode.next = node(key, value);
+  }
 
   return {
     node, set, get, has, buckets, calculateLoadFactor,
@@ -73,13 +78,38 @@ const HashMap = () => {
 const hashMap = HashMap();
 hashMap.set('mario', 'is good');
 hashMap.set('luigi', 'is also good');
-hashMap.set('jehrek', 'is also good');
 hashMap.set('john', 'is also good');
+hashMap.set('jehrek', 'is also good');
 hashMap.set('berek', 'is awaiting something');
 hashMap.set('samson', 'is awaiting something');
 hashMap.set('sami', 'is ok');
 hashMap.set('cmtdandebor', 'is feeling thirsty');
 hashMap.set('salam', 'can\'t wait no more');
 hashMap.set('jeremy', 'Star align is the best game ever done on roblox');
+hashMap.set('sarah', 'Star align is the best game ever done on roblox');
+hashMap.set('eloise', 'ok bro');
+hashMap.set('nathan', 'lipsum orelum');
+hashMap.set('layboy', 'such legacy can\'t exist');
+hashMap.set('felicity', 'i love drawing');
+hashMap.set('solomon', 'everytime someone call me saumon i speak french');
+hashMap.set('greek', 'not roman');
+hashMap.set('Yousouf', 'AAAARGGGHHH');
+hashMap.set('dayumTheGreatest', 'When i smile, it\'s a bad sign 👹');
+hashMap.set('carti', 'if i\'m a b* im the baddest b*');
+hashMap.set('mightyKingdom', 'i\'m glorious');
+hashMap.set('luffy', 'orewa Monkey D. Luffy');
+hashMap.set('todoum', 'loudoum');
+hashMap.set('sachiburidana', 'mugiwara');
+hashMap.set('todou', 'loudou');
+hashMap.set('sachiburiana', 'mugiwaa');
+hashMap.set('todo', 'loudo');
+hashMap.set('sachiburian', 'mugiwa');
+hashMap.set('tod', 'loud');
+hashMap.set('sachiburia', 'mugiw');
+hashMap.set('to', 'lou');
+hashMap.set('sachiburi', 'mugi');
+hashMap.set('fecity', 'i love dring');
+hashMap.set('solom', 'i speak french');
 
-console.log(hashMap.get('jeremy'));
+console.log(hashMap.buckets);
+console.log(hashMap.calculateLoadFactor());
